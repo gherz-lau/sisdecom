4c2319f72daa910cb8dbb706b2a4928feb69d57f
Larry Gher <larry.gher@gmail.com>
2020-10-24 10:34:18 -0600

    creadas dos paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FilebaseComponent } from './pages/filebase/filebase.component';
+import { CaptureComponent } from './pages/capture/capture.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilebaseComponent,
+    CaptureComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/filebase/filebase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filebase',
+  templateUrl: './filebase.component.html',
+  styleUrls: ['./filebase.component.scss']
+})
+export class FilebaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
