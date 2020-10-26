8ee78fe1f16bc023c06eb971df105420bff09df8
Manuel Gonzalez <manuel_agh@hotmail.com>
2020-10-25 19:17:26 -0700

    fix search function

--- src/app/pages/capture/capture.component.ts
+++ src/app/pages/capture/capture.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AcuseCobro, Pagination } from 'src/app/core/models';
-import { GlobalService, PaginationService, ExporterService } from 'src/app/core/services';
+import {
+  GlobalService,
+  PaginationService,
+  ExporterService,
+} from 'src/app/core/services';
 
 @Component({
   selector: 'app-capture',
   templateUrl: './capture.component.html',
-  styleUrls: ['./capture.component.scss']
+  styleUrls: ['./capture.component.scss'],
 })
-
 export class CaptureComponent implements OnInit {
   //total de acuses
   content: Array<AcuseCobro> = new Array<AcuseCobro>();
@@ -20,45 +24,53 @@ export class CaptureComponent implements OnInit {
   pagination: Pagination<AcuseCobro> = new Pagination<AcuseCobro>();
   active = 1;
 
-
-  constructor(private globalService: GlobalService, private paginationService: PaginationService, private excelService: ExporterService) {
+  constructor(
+    private globalService: GlobalService,
+    private paginationService: PaginationService,
+    private excelService: ExporterService,
+    private router: Router
+  ) {
     this.content = globalService.acuseCobroContent;
     this.setPage();
   }
   ngOnInit(): void {
+    if (!this.content || this.content.length == 0) this.router.navigate(['/']);
   }
-
 
   search($event) {
     if ($event.keyCode === 13) {
-      let index = this.content.findIndex(f => {
+      let index = this.content.findIndex((f) => {
         return f.folioFormato.toString() === this.acuseSearch;
       });
 
       let element = this.content[index];
       this.aCapturados.push(element);
-      this.content = this.content.filter(f => f.folioFormato.toString() !== this.acuseSearch);
+      this.content = this.content.filter(
+        (f) => f.folioFormato.toString() !== this.acuseSearch
+      );
+      this.setPage();
       this.acuseSearch = '';
     }
   }
 
   //info de paginacion
   setPage() {
-    this.pagination.pager = this.paginationService.getPager(this.content.length, this.pagination.page, this.pagination.pageSize);
-    this.pagination.pagedItems = this.content.slice(this.pagination.pager.startIndex, this.pagination.pager.endIndex + 1);
+    this.pagination.pager = this.paginationService.getPager(
+      this.content.length,
+      this.pagination.page,
+      this.pagination.pageSize
+    );
+    this.pagination.pagedItems = this.content.slice(
+      this.pagination.pager.startIndex,
+      this.pagination.pager.endIndex + 1
+    );
   }
   newPage($event) {
     this.pagination.page = $event;
     this.setPage();
   }
 
-  exportAsXLSX(): void {
-    this.excelService.exportToExcel(this.content, 'my_export');
+  exportAsXLSX(collection): void {
+    this.excelService.exportToExcel(collection, 'my_export');
   }
-
-
-
-
-
-
 }
